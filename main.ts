7cc9153cc6c74920f92e555834118a2a96df86e8
Maha_Zaidi <mraaj130@gmail.com>
2024-03-15 07:48:28 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,27 @@
+// BMI Calculator (Body Mass Index)
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+const questions = [
+    {
+        type: "number",
+        name: "weight",
+        message: "How much your Weight(kg)?"
+    },
+    {
+        type: "number",
+        name: "height",
+        message: "How much your Height(m)?"
+    }
+];
+
+const calculate_BMI = (weight:number, height:number) =>
+weight / (height * height);
+const emo1 = "🏋️‍♀️";
+const emo2 ="💪";
+
+const main = async () => {
+      const{weight,height} = await inquirer.prompt(questions);
+      console.log(`BMI: ${chalk.bgMagenta.bold.italic (calculate_BMI(weight,height).toFixed(2))} ${emo1 + emo2}`);
+}
+main();
